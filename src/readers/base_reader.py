517cd53c51aad069f4725467f7e7101147e1e6ce
Fabián Murgado <fabian@fabianmurgado.com>
2026-02-10 18:02:44 +0100

    feat(readers): añadir reader base abstracto

---
+++ src/readers/base_reader.py
@@ -0,0 +1,11 @@
+
+from abc import ABC, abstractmethod
+from typing import Iterator
+
+class BaseReader(ABC):
+
+    @abstractmethod
+    def read(self, file_path: str) -> Iterator[str]:
+        pass
+
+
