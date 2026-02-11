dd4506d5198f3e97b8cbf0804622b92b5dc11c90
Fabián Murgado <fabian@fabianmurgado.com>
2026-02-11 16:03:06 +0100

    test(extractors): add URL extractor tests

--- tests/test_extractors.py
+++ tests/test_extractors.py
@@ -1,6 +1,6 @@
 """Tests for the extractors module."""
 
-from textkit.extractors import DateExtractor
+from textkit.extractors import DateExtractor, URLExtractor
 
 
 class TestDateExtractor:
@@ -21,3 +21,25 @@ class TestDateExtractor:
         extractor = DateExtractor()
         dates = extractor.extract(text)
         assert dates == ["01/01/2024", "31/12/2024"]
+
+
+class TestURLExtractor:
+    def test_extract_urls1(self):
+        text = (
+            "Visita nuestro sitio web en https://www.example.com o http://example.org."
+        )
+        extractor = URLExtractor()
+        urls = extractor.extract(text)
+        assert urls == ["https://www.example.com", "http://example.org"]
+
+    def test_extract_urls2(self):
+        text = "No hay URLs en este texto."
+        extractor = URLExtractor()
+        urls = extractor.extract(text)
+        assert urls == []
+
+    def test_extract_urls3(self):
+        text = "URLs en diferentes formatos: www.example.com, example.com/path/to/resource."
+        extractor = URLExtractor()
+        urls = extractor.extract(text)
+        assert urls == ["www.example.com", "example.com/path/to/resource"]
