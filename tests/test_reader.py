65efc2d25c590e458e1c73b1bb7631f7b6fb8b90
Fabián Murgado <fabian@fabianmurgado.com>
2026-02-16 13:04:17 +0100

    doc: fix reader tests comments

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -7,6 +7,9 @@ from textkit.reader import Reader
 
 @pytest.fixture
 def sample_txt_file(tmp_path: Path) -> Path:
+    """
+    Creates a sample .txt file for testing purposes and returns its path.
+    """
     file_path = tmp_path / "sample_document.txt"
     file_content = (
         "Yesterday, all my troubles seemed so far away\n"
@@ -18,6 +21,9 @@ def sample_txt_file(tmp_path: Path) -> Path:
 
 
 def test_read_txt(sample_txt_file: Path):
+    """
+    Test the _read_txt method of the Reader class.
+    """
     reader = Reader(str(sample_txt_file))
     lines = list(reader._read_txt())
 
@@ -35,7 +41,7 @@ def test_unsupported_format():
 
 def test_file_not_found():
     """
-    If _read_txt function raises a FileNotFoundError when the specified file does not exist.
+    Test if _read_txt function raises a FileNotFoundError when the specified file does not exist.
     """
     with pytest.raises(FileNotFoundError, match=r"File not found: non_existent\.txt"):
         gen = Reader("non_existent.txt")._read_txt()
@@ -44,9 +50,7 @@ def test_file_not_found():
 
 def test_wrong_extension_txt(sample_txt_file: Path):
     """
-    If _read_text function raises a ValueError when the file does not have a .txt extension.
-    :param sample_txt_file: A temporary .txt file created by the fixture.
-    :type sample_txt_file: Path
+    Test if _read_txt function raises a ValueError when the file does not have a .txt extension.
     """
     with pytest.raises(ValueError, match=r"File must have a \.txt extension"):
         gen = Reader(str(sample_txt_file.with_suffix(".md")))._read_txt()
